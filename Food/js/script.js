5f0aa6a557ffd93f1eecdda608137b529edf5f73
Mldorlfuse <74578680+Mldorlfuse@users.noreply.github.com>
2024-03-10 19:32:09 +0300

    Add files via upload

---
+++ Food/js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import timer from './modules/timer';
+import cards  from './modules/cards';
+import forms from './modules/forms';
+import slider from './modules/slider';
+import calc from './modules/calc';
+
+document.addEventListener('DOMContentLoaded', () => {
+    tabs();
+    modal();
+    timer();
+    cards();
+    forms();
+    slider();
+    calc();
+});
+
+
